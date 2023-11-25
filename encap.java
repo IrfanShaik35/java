9aeb39d0fc38e12ea5ce1f5ca432c754129a56c3
IrfanShaik35 <132468935+IrfanShaik35@users.noreply.github.com>
2023-11-24 21:35:41 -0800

    Add files via upload

---
+++ encap.java
@@ -0,0 +1,22 @@
+class animal {
+// private field 
+private int age;
+//getter method 
+Public int getage ( ) {
+return age;
+}
+//setter method 
+public void setAge ( int age ) {
+this. Age = age;
+}
+}
+class Main {
+public static void main (String args []);
+//create an object of person 
+Animal a1= new Animal ();
+//change age using setter 
+A1. setAge (12);
+// access age using getter 
+System.out.println(“ animal age is ” + a1. getage ( ) );
+}
+}
