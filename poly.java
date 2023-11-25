eb39cf9cf4840fa603b670d57bbe20956c282d52
IrfanShaik35 <132468935+IrfanShaik35@users.noreply.github.com>
2023-11-24 21:34:05 -0800

    Add files via upload

---
+++ poly.java
@@ -0,0 +1,20 @@
+public class Bird {
+…
+Public void sound ( ) {
+System.out.println ( “ birds sounds “ );
+}
+}
+public class pigeon extends Bird {
+…
+@override
+public void sound ( ) {
+System.out.println( “ cooing ” ) ;
+}
+}
+public class sparrow extends Bird ( ) {
+….
+@override
+Public void sound ( ){
+System.out.println( “ chip ” ) ;
+}
+}
